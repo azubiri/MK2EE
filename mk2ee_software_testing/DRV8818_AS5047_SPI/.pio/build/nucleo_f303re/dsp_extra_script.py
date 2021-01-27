5d8e92ef474a26f9a886043a4dc0d11158fb20c1
Alberto Zubiri <azubiri@protonmail.com>
2021-01-27 19:19:15 +0100

    gitignores files are added into software testing subfolders, and vscode and pio files are deleted

--- mk2ee_software_testing/DRV8818_AS5047_SPI/.pio/build/nucleo_f303re/dsp_extra_script.py
+++
@@ -1,2 +0,0 @@
-Import('env')
-env.Prepend(CPPPATH=['C:/Users/azubc/.platformio/packages/framework-mbed@6.51504.200716/features/unsupported/dsp', 'C:/Users/azubc/.platformio/packages/framework-mbed@6.51504.200716/features/unsupported/dsp/cmsis_dsp', 'C:/Users/azubc/.platformio/packages/framework-mbed@6.51504.200716/features/unsupported/dsp/dsp'])
